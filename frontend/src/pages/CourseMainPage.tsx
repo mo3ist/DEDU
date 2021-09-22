a98859ec511a74c31e9d2ef1f4fb22c3671d8636
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-09-22 21:28:01 +0200

    fix: react-ts hello world component

---
+++ frontend/src/pages/CourseMainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+interface Props {
+
+}
+
+export const HelloWorld: React.FC<Props> = () => {
+	return (
+		<div>
+			<p>Hello World from Course Main Page</p>
+		</div>
+	)
+}
