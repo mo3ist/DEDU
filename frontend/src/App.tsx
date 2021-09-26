eda4d8afe4573c35b20c4dd0e78ddb29beb7c6e2
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-09-26 02:47:00 +0200

    feat: fetch hello world - ApolloClient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { HelloWorld } from './pages/CourseMainPage'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+	uri: 'http://127.0.0.1:8000/graphql/',
+	cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <HelloWorld />
+	  <ApolloProvider client={ client }>
+		  <HelloWorld />
+	  </ApolloProvider>
   );
 }
 
