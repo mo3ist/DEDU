39d12c38a043b573eef3071c076c9c7d4ccf12cf
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-12-01 03:39:33 +0200

    fix: use postgresql

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3',)	# It errored out all of a sudden, str solved it. 
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': str(BASE_DIR / 'db.sqlite3',)	# It errored out all of a sudden, str solved it. 
+    # }
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
