8c72142d68caa4da46f4222eb4b7120304b6f400
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-12-22 21:36:57 +0200

    fix: add minor 'vote' fixes

--- frontend/src/common/components/vote/vote.tsx
+++ frontend/src/common/components/vote/vote.tsx
@@ -57,7 +57,7 @@ const Vote: React.FC<Props> = ({ contentId, voteCount, userVote }) => {
 
 	return (
 		<div
-			className="flex flex-col w-8 md:w-10 items-center justfiy-center text-secondary-200"
+			className="flex flex-col w-8 md:w-10 items-center justfiy-center text-primary-100"
 		>
 			{/* upvote */}
 			<ProtectedButton
@@ -69,9 +69,9 @@ const Vote: React.FC<Props> = ({ contentId, voteCount, userVote }) => {
 						}
 					})
 				}}
-				className={classname("text-primary border border-primary rounded-lg", {"rounded-lg bg-primary text-secondary-100": userVote === "UPVOTE"})}
+				className={classname("text-primary border border-primary rounded-lg md:w-10", {"rounded-lg bg-primary text-secondary-100": userVote === "UPVOTE"})}
 			>
-				<svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7 md:h-10 md:w-10" viewBox="0 0 20 20" fill="currentColor">
+				<svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7 md:h-9 md:w-9" viewBox="0 0 20 20" fill="currentColor">
 					<path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
 				</svg>
 			</ProtectedButton>
@@ -92,7 +92,7 @@ const Vote: React.FC<Props> = ({ contentId, voteCount, userVote }) => {
 				}}
 				className={classname("text-primary border border-primary rounded-lg", {"rounded-lg bg-primary text-secondary-100": userVote === "DOWNVOTE"})}
 			>
-				<svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7 md:h-10 md:w-10" viewBox="0 0 20 20" fill="currentColor">
+				<svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7 md:h-9 md:w-9" viewBox="0 0 20 20" fill="currentColor">
 					<path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
 				</svg>
 			</ProtectedButton>
