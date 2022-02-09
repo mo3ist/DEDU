09b528311cedd40fc2d8a9cfcdb85f45b27c3e54
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-09 16:29:49 +0200

    fix: put SECRET_KEY and DEBUG into the .env file

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^=ruj4l_qs-vrm4oor7o(zb6k90rj)#$1z$u%bw(jkva^0%4yw'
+SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
