e3612993f392191765c9a4f1487f0a1993031cbf
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-09 16:07:24 +0200

    feat: create production docker env -init

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 	path('social/', include('social_django.urls', namespace='social')),
-]
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
